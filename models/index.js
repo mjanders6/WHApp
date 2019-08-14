c8b3e2cd9372d8416b66e638764e889fdb51a006
Mike <mjanders6@gmail.com>
2019-08-13 21:17:25 -0700

    clean up models index

--- models/index.js
+++ models/index.js
@@ -4,12 +4,6 @@ const sequelize = require('../config')
 let Purchaseorder = require('./Purchaseorder.js')(sequelize, Sequelize)
 let User = require('./User.js')(sequelize, Sequelize)
 
-// relationships
-// User.hasMany(List)
-// List.belongsTo(User)
-// List.hasMany(Item)
-// Item.belongsTo(List)
-
 User.hasMany(Purchaseorder)
 Purchaseorder.belongsTo(User)
 
